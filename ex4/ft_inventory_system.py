0f2b398da04b07d8d5a37d7eaccd906f02c44320
bapotuse <beauxbaptiste@gmail.com>
2026-03-07 22:08:34 +0100

    Fonction items_categories a finir (l.56)

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,37 +1,77 @@
 import sys
 
 
-if __name__ == "__main__":
-    print("=== Inventory System Analysis ===")
-    temp_tab = []
+def parse_inventory(arguments):
     dictionary = dict()
-    total_items = 0
-
-    # parser les elements dans le dictionnaire
-    for element in sys.argv[1:]:
+    for element in arguments:
         temp_tab = element.split(':')
-        dictionary[temp_tab[0]] = temp_tab[1]
+        dictionary[temp_tab[0]] = int(temp_tab[1])
+    return dictionary
 
-    # savoir le nombre d'items
+
+def count_total_items(dictionary):
+    total_items = 0
     for element in dictionary.values():
-        total_items += int(element)
-    print(f'Total items in inventory:   {total_items}')
+        total_items += element
+    return total_items
 
-    # savoir la taille des elements
+
+def print_summary(dictionary, total_items):
+    print(f'Total items in inventory:   {total_items}')
     different_items = len(dictionary.keys())
     print(f'Unique item types:  {different_items}\n')
 
+
+def print_inventory(dictionary, total_items):
     print("=== Current inventory ===")
     for element in sorted(dictionary, key=dictionary.get, reverse=True):
-        value = int(dictionary[element])
-        purcentage = float(value / total_items * 100)
-        purcentage = format(purcentage, ".1f")
+        value = dictionary[element]
+        percentage = float(value / total_items * 100)
+        percentage = format(percentage, ".1f")
         if value == 1:
-            print(f'{element}: {dictionary[element]} unit ({purcentage}%)')
+            print(f'{element}: {dictionary[element]} unit ({percentage}%)')
         elif value > 1:
-            print(f'{element}: {dictionary[element]} units ({purcentage}%)')
+            print(f'{element}: {dictionary[element]} units ({percentage}%)')
 
+
+def print_statistics(dictionary):
     print("\n=== Inventory Statistics ===")
-    for element in dictionary.keys():
-        
+    most = max(dictionary, key=dictionary.get)
+    least = min(dictionary, key=dictionary.get)
 
+    if dictionary[most] > 1:
+        most_unit = "units" 
+    else:
+        most_unit = "unit"
+
+    if dictionary[least] > 1:
+        least_unit = "units"
+    else:
+        least_unit = "unit"
+
+    print(f"Most abundant: {most} ({dictionary[most]} units)")
+    print(f"Least abundant: {least} ({dictionary[least]} units)")
+
+## A CONTINUER
+def items_categories(dictionary):
+    print("=== Item Categories ===")
+    scarce_elements = dict()
+    moderate_elements = dict()
+    for element in sorted(dictionary, key=dictionary.get, reverse=True):
+        if dictionary[element] <= 3:
+            scarce_elements[] 
+        else:
+            moderate_elements.append(dictionary[element])
+
+
+
+if __name__ == "__main__":
+    print("=== Inventory System Analysis ===")
+
+    dictionary = parse_inventory(sys.argv[1:])
+    total_items = count_total_items(dictionary)
+
+    print_summary(dictionary, total_items)
+    print_inventory(dictionary, total_items)
+    print_statistics(dictionary)
+    items_categories(dictionary)
