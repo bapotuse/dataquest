24df8c8a3dce68c9d48961904c96cab5a3de34dd
Baptiste Beaux <bbeaux@c2r9s1.marseille.42.school>
2026-03-11 18:21:47 +0100

    ex5 et ex0

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -2,19 +2,23 @@ import sys
 
 
 if __name__ == "__main__":
-    final_list = []
-    length = len(sys.argv)
-    count = 1
-    for element in sys.argv:
-        final_list.append(element)
     print("=== Command Quest ===")
-    if length == 1:
+    try:
+        if len(sys.argv) > 1:
+            final_list = []
+            length = len(sys.argv)
+            count = 1
+            for element in sys.argv:
+                final_list.append(element)
+            print(f'Program name:   {sys.argv[0]}')
+            print(f'Arguments received:  {length - 1}')
+            for argument in sys.argv[1:]:
+                print(f'Argument {count}:   {argument}')
+                count += 1
+            print(f'Total arguments:    {length}')
+        else:
+            raise ValueError
+    except ValueError:
         print("No arguments provided!")
         print(f'Program name:   {sys.argv[0]}')
-    else:
-        print(f'Program name:   {sys.argv[0]}')
-        print(f'Arguments received:  {length - 1}')
-        for argument in sys.argv[1:]:
-            print(f'Argument {count}:   {argument}')
-            count += 1
-    print(f'Total arguments:    {length}')
+        print("Total arguments: 1")
